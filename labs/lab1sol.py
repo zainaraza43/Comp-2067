04baa1dde241c2159107c6c984a7751da193f291
zain <urboyzain@gmail.com>
2022-01-17 15:53:08 -0500

    final commit before final

--- labs/lab1sol.py
+++ labs/lab1sol.py
@@ -12,4 +12,3 @@ print("x+y-2=",answer) # printing answer
 
 animal = input("Please enter your favourite animal:") # user input for favourite animal
 print("Your favourite animal is:",animal) # printing user's favourite animal
-
