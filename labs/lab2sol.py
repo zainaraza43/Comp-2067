f80026cb20eb068f0b416e4d702bdebda03bf812
zain <urboyzain@gmail.com>
2022-01-17 16:04:36 -0500

    fix lab2

--- labs/lab2sol.py
+++ labs/lab2sol.py
@@ -2,7 +2,7 @@
 # Comp 2067 - Winter 2022
 
 x = 3 # assigning x to 3
-print("x=",x) # printing x
+print("x =",x) # printing x
 
 x = float(x) # converting x to a float
 print("After converting to float: x =",x) # printing x after conversion
@@ -13,7 +13,11 @@ print("y=",y) # printing y value
 z = y ** x # assigning z to y to the power of x
 print("The value of y**x is: z =",z) # printing z value
 
-print("The absolute vaalue of z is:",round(z, 3)) # printing to 3 decimal places
+z = round(z, 3) # rounding z to 3 decimal places and storing it in z
+
+print("The value of z rounded to 3 decimal places =",z) # printing z
+
+print("The absolute value of z is:",abs(z)) # printing the absolute value of z
 
 result = (x*y + 7)/(x + y) # Assigning result to the equation
 
